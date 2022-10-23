1db1bf1f442ab8ed7dc3899a28f4d65781570b72
Mauro <dantuchiii@gmail.com>
2022-10-23 15:52:49 -0300

    v1.0

---
+++ components/readTasks.js
@@ -0,0 +1,28 @@
+import { uniqueDates, orderDates } from "../services/date.js";
+import { createTask } from "./addTask.js";
+import dateElement from "./dateElement.js";
+
+export const displayTasks = () => {
+    
+    const taskList = JSON.parse(localStorage.getItem('task')) || [];
+    const list = document.querySelector('[data-list]');   
+    const dates = uniqueDates(taskList);    
+    orderDates(dates);
+    
+    dates.forEach( date => {        
+        const dateMoment = moment(date, "DD/MM/YYYY");
+        list.appendChild(dateElement(date));
+        taskList.forEach( (task) => {   
+            const taskDate = moment(task.dateFormat, "DD/MM/YYYY");  
+            const diff = dateMoment.diff(taskDate);
+            console.log(diff);       
+
+            if (diff === 0) {
+                list.appendChild(createTask(task));
+            } 
+    
+        });
+    });
+
+    
+}
